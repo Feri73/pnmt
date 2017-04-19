704670c44dd0a0536666784854765448336cf2ff
Faraz Yazdani <ferisampad@gmail.com>
2017-04-19 11:07:46 +0430

    added batch support to corpus

--- corpus.py
+++ corpus.py
@@ -10,32 +10,54 @@ import os.path
 
 class Corpus:
     def __init__(self, config):
-        # self.EOS_PUNCS = config.eos_puncs or ['.', '!', '?', '\n']
         self.PUNCS = config.puncs or list(string.punctuation) + [' ', '\n']
         self.SAVE_DIR = config.save_dir
         self.name = config.name
         self.data_addr = config.data_addr
         self.vocab_size = config.vocab_size
         self.encoding = config.encoding or 'ascii'
+        self.divs_size = config.divs_size
 
-        self.words = None
-        self.tokens = None
-        self.data = None
-        self.index = None
+        self.words = []
+        self.tokens = []
+        self.datas = []
+        self.indexes = []
+        self.sizes = []
+        self.div_index = 0
 
         self.load_data()
 
-    # def find_sentences(self, str):
-    #     sentences = []
-    #     tmp = ''
-    #     for c in str:
-    #         tmp = tmp + c
-    #         if c in self.EOS_PUNCS:
-    #             sentences.append(tmp)
-    #             tmp = ''
-    #     if len(tmp) > 1:
-    #         sentences.append(tmp)
-    #     return sentences
+    def load_data(self):
+        path = self.SAVE_DIR + '\\' + self.name
+        if not os.path.exists(self.SAVE_DIR):
+            os.makedirs(self.SAVE_DIR)
+        if os.path.isfile(path + 'tokens'):
+            self._load_property('tokens')
+            self._load_property('words')
+            self._load_property('datas')
+            self._load_property('indexes')
+            self._load_property('sizes')
+            self._load_property('div_index')
+            self.vocab_size = len(self.words)
+        else:
+            file = codecs.open(self.data_addr, 'r', self.encoding)
+            sentences = []
+            for line in file.readlines():
+                sentences.append(line[0:-1])
+            file.close()
+            self.tokens, sentences = self.find_tokens(sentences)
+            self.vocab_size = min(self.vocab_size, len(self.tokens))
+            self.datas, self.sizes = self.tokenize(sentences)
+            self.words = [x[0] for x in sorted(list(self.tokens.items()), key=lambda x: x[1])]
+            self.indexes = [0 for _ in self.divs_size]
+            self.div_index = 0
+            self._save_property('tokens')
+            self._save_property('words')
+            self._save_property('datas')
+            self._save_property('indexes')
+            self._save_property('sizes')
+            self._save_property('div_index')
+        return self.vocab_size
 
     def find_words(self, str):
         words = []
@@ -71,93 +93,51 @@ class Corpus:
         tokens['UNK'] = len(tokens)
         return tokens, sents
 
-    def load_data(self):
-        path = self.SAVE_DIR + '\\' + self.name
-        if not os.path.exists(self.SAVE_DIR):
-            os.makedirs(self.SAVE_DIR)
-        if os.path.isfile(path + 'tokens'):
-            with open(path + 'tokens', 'rb') as file:
-                self.tokens = pickle.load(file)
-            with open(path + 'words', 'rb') as file:
-                words = pickle.load(file)
-            with open(path + 'data', 'rb') as file:
-                data = pickle.load(file)
-            with open(path + 'index', 'rb') as file:
-                index = pickle.load(file)
-            self.vocab_size = len(words)
-        else:
-            file = codecs.open(self.data_addr, 'r', self.encoding)
-            sentences = []
-            for line in file.readlines():
-                # sentences.extend(self.find_sentences(line[0:-1]))
-                sentences.append(line[0:-1])
-            file.close()
-            self.tokens, sentences = self.find_tokens(sentences)
-            self.vocab_size = min(self.vocab_size, len(self.tokens))
-            data = self.tokenize(sentences)
-            words = [x[0] for x in sorted(list(self.tokens.items()), key=lambda x: x[1])]
-            index = 0
-            with open(path + 'tokens', 'wb+') as file:
-                pickle.dump(self.tokens, file)
-            with open(path + 'words', 'wb+') as file:
-                pickle.dump(words, file)
-            with open(path + 'data', 'wb+') as file:
-                pickle.dump(data, file)
-            with open(path + 'index', 'wb+') as file:
-                pickle.dump(index, file)
-        self.words = words
-        self.data = data
-        self.index = index
-        return self.vocab_size
-
-    def next_batch(self, size, save=False):  # poor performance
-        d_size = len(self.data)
-        assert size <= d_size
-        end = (self.index + size) % d_size
-        # i=self.index
-        # size=self.data
-        # result=[]
-        # while(i!=end):
-
-
-        # extend for size
-
-
-        if end <= self.index:
-            tmp1 = self.data[self.index:]
-            tmp2 = self.data[0:end]
-            # if tmp1.shape[0] == 0:
-            #     res = tmp2
-            # elif tmp2.shape[0] == 0:
-            #     res = tmp1
-            # else:
-            res = tmp1 + tmp2
-        else:
-            res = self.data[self.index:end]
-        self.index = end
-        if save:
-            self.save_corpus_state()
-        return np.array(res, dtype='float32')
-
     def tokenize(self, sentences):
-        result = []
+        sizes = [len(s) for s in sentences]
+        mmax = max(sizes)
+        self.divs_size = self.divs_size + [mmax] if mmax > self.divs_size[-1] else self.divs_size
+        inds = [[] for _ in range(len(self.divs_size))]
         for i, s in enumerate(sentences):
-            mm = len(s)
-            tmp = np.zeros([mm, self.vocab_size], dtype='float32')
-            for j in range(0, mm):
-                w = s[j]  # if j < len(s) else 'EOS'
-                if w in self.tokens:
-                    tmp[j, self.tokens[w]] = 1
-                else:
-                    tmp[j, self.tokens['UNK']] = 1
-            result.append(tmp)
-        return result
+            for j, n in enumerate(self.divs_size):
+                if len(s) <= n:
+                    inds[j].append(i)
+                    break
+        result = [np.zeros([len(inds[j]), n, self.vocab_size], dtype='float32') for j, n in enumerate(self.divs_size)]
+        for j, n in enumerate(self.divs_size):
+            for i, si in enumerate(inds[j]):
+                for k in range(n):
+                    w = sentences[si][k] if k < len(sentences[si]) else 'EOS'
+                    result[j][i, k, self.tokens[w]] = 1
+        return result, sizes
 
     def save_corpus_state(self):
-        path = self.SAVE_DIR + '\\' + self.name
-        with open(path + 'index', 'wb+') as file:
-            pickle.dump(self.index, file)
+        self._save_property('indexes')
+        self._save_property('div_index')
 
     def to_words(self, token_list):
         numbers = [np.argmax(x) for x in token_list]
         return ' '.join([self.words[i] for i in numbers])
+
+    def next_batch(self, size, save=False):  # poor performance
+        while len(self.datas[self.div_index]) == 0:
+            self.div_index = (self.div_index + 1) % len(self.divs_size)
+        d_size = len(self.datas[self.div_index])
+        size = min(size, d_size)
+        start = self.indexes[self.div_index]
+        end = (start + size) % d_size
+        res = self.datas[self.div_index][start:end]
+        self.indexes[self.div_index] = end
+        self.div_index = (self.div_index + 1) % len(self.divs_size)
+        if save:
+            self.save_corpus_state()
+        return res
+        # return np.array(res, dtype='float32')
+
+    def _save_property(self, p_name):
+        with open(self.SAVE_DIR + '\\' + self.name + p_name, 'wb+') as file:
+            pickle.dump(getattr(self, p_name), file)
+
+    def _load_property(self, p_name):
+        with open(self.SAVE_DIR + '\\' + self.name + p_name, 'rb') as file:
+            setattr(self, p_name, pickle.load(file))
--- tmp.py
+++ tmp.py
@@ -1,19 +1,26 @@
 from bunch import Bunch
 from corpus import Corpus
 
-fa_corpus_config = Bunch()
-fa_corpus_config.save_dir = 'F:\\Faraz\\University\\Thesis\\pnmt\\cache\\data'
-fa_corpus_config.data_addr = 'F:\\Faraz\\University\\Thesis\\pnmt\\data\\fa1'
-fa_corpus_config.name = 'fa_corpus2'
-fa_corpus_config.vocab_size = 500
-fa_corpus_config.encoding = 'utf-8'
-fa_corpus = Corpus(fa_corpus_config)
+# fa_corpus_config = Bunch()
+# fa_corpus_config.save_dir = 'F:\\Faraz\\University\\Thesis\\pnmt\\cache\\data'
+# fa_corpus_config.data_addr = 'F:\\Faraz\\University\\Thesis\\pnmt\\data\\fa1'
+# fa_corpus_config.name = 'fa_corpus2'
+# fa_corpus_config.vocab_size = 500
+# fa_corpus_config.encoding = 'utf-8'
+# fa_corpus = Corpus(fa_corpus_config)
 
 en_corpus_config = Bunch()
 en_corpus_config.save_dir = 'F:\\Faraz\\University\\Thesis\\pnmt\\cache\\data'
-en_corpus_config.data_addr = 'F:\\Faraz\\University\\Thesis\\pnmt\\data\\en1'
-en_corpus_config.name = 'en_corpus2'
-en_corpus_config.vocab_size = 500
+en_corpus_config.data_addr = 'F:\\Faraz\\University\\Thesis\\pnmt\\data\\test'
+en_corpus_config.name = 'test'
+en_corpus_config.vocab_size = 100
+en_corpus_config.divs_size = [5, 8, 11, 17]
 en_corpus = Corpus(en_corpus_config)
 
-print(fa_corpus)
+while(True):
+    input('next_batch')
+    for s in en_corpus.next_batch(5):
+        print(en_corpus.to_words(s))
+
+
+print('fin')
